2dd55c6c1d8b5d99f9e9cf752827bcdf109c4a88
10241367 <10241367@D205PC30>
2025-03-17 14:05:52 +0300

    gfggfgfgfgffg

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -56,7 +56,7 @@ namespace WebApplication2.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,CategoryName")] Category category)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 _context.Add(category);
                 await _context.SaveChangesAsync();
@@ -93,7 +93,7 @@ namespace WebApplication2.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 try
                 {
